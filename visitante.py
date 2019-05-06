d5f4abdfc0a3fb305a2779d752ab058618da752a
MauricioFIUBA <mlcabrera@fi.uba.ar>
2019-05-06 14:59:03 -0300

    Cambio en los espacios para que pep8 no se queje

--- visitante.py
+++ visitante.py
@@ -1,21 +1,18 @@
 class Visitante:
-    def __init__(self,nombre, hora_entrada, tiempo_permanencia):
+    def __init__(self, nombre, hora_entrada, tiempo_permanencia):
         self.__nombre = nombre
-        self.__entrada = int (hora_entrada)
+        self.__entrada = int(hora_entrada)
         self.__permanencia = int(tiempo_permanencia)
         self.__salida = self.__entrada + self.__permanencia
 
-
     def get_nombre(self):
         return self.__nombre
-
 
     def get_entrada(self):
         return self.__entrada
 
     def get_permanencia(self):
-       return self.__permanencia
-
+        return self.__permanencia
 
     def get_salida(self):
         return self.__salida
